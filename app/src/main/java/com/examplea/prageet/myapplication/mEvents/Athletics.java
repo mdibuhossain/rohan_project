d032ace2d6245d7fc960e257b3e9d57320a50be0
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-05-04 01:50:58 +0600

    updated

--- app/src/main/java/com/examplea/prageet/myapplication/mEvents/Athletics.java
+++ app/src/main/java/com/examplea/prageet/myapplication/mEvents/Athletics.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
